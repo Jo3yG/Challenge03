47eac04d5537c0536e9a5cf2d4cdc45e60733cb3
Jo3yG <nonuser31@yahoo.com>
2023-09-07 17:04:33 -0500

    created variables

--- script.js
+++ script.js
@@ -20,11 +20,10 @@ var generateBtn = document.querySelector("#generate");
 
 
 // setting up variables
-    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    var numeral = "0123456789";
-    var special = "!@#$%^&*()";
-    var click
+    var upperCase = ["A""B""C""D""E""F""G""H""I""J""K""L""M""N""O""P""Q""R""S""T""U""V""W""X""Y""Z"];
+    var lowerCase = ["a""b""c""d""e""f""g""h""i""j""k""l""m""n""o""p""q""r""s""t""u""v""w""x""y""z"];
+    var numeral = ["0""1""2""3""4""5""6""7""8""9"];
+    var special = ["!""@""#""$""%""^""&""*""("")"];
     var selections
 //need to create function to generate password
     function generatePassword(){
